1a56e5a3f5e30ac011c5d6ee64988c5763a756a5
Frank <xuzy213@gmail.com>
2018-07-24 11:06:51 +1200

    code refactor

--- 3-longestSubstring.js
+++ 3-longestSubstring.js
@@ -3,29 +3,42 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLongestSubstring = function(s: string) {
+//   let count = 0
+//   let newS = s
+//   let tempStr = ""
+//   if(s === "") return 0
+//   if(s.length === 1) return 1
+//   while(newS.length > count) {
+//     for(let i = 0; i < newS.length; i++) {
+//       if(tempStr.indexOf(newS[i]) === -1) {
+//         tempStr += newS[i]        
+//         if(count < tempStr.length){
+//           count = tempStr.length
+//         }
+//       } else {
+//         newS = newS.substr(tempStr.indexOf(newS[i]) + 1)
+//         tempStr = ""
+//         break
+//       } 
+//     }
+//   }
+//   return count
+// };
 var lengthOfLongestSubstring = function (s) {
-    var count = 0;
-    var newS = s;
-    var tempStr = "";
-    if (s === "")
-        return 0;
-    if (s.length === 1)
-        return 1;
-    while (newS.length > count) {
-        for (var i = 0; i < newS.length; i++) {
-            if (tempStr.indexOf(newS[i]) === -1) {
-                tempStr += newS[i];
-                if (count < tempStr.length) {
-                    count = tempStr.length;
-                }
-            }
-            else {
-                newS = newS.substr(tempStr.indexOf(newS[i]) + 1);
-                tempStr = "";
-                break;
-            }
+    var i = 0, j = 0, max = 0, map = {};
+    var n = s.length;
+    while (i < n && j < n) {
+        if (map[s[j]] === undefined) {
+            map[s[j]] = 1;
+            j++;
+            max = Math.max(max, j - i);
+        }
+        else {
+            delete map[s[i]];
+            i++;
         }
     }
-    return count;
+    return max;
 };
-console.log(lengthOfLongestSubstring('ccc'));
+console.log(lengthOfLongestSubstring('pwwkew'));
--- 3-longestSubstring.ts
+++ 3-longestSubstring.ts
@@ -2,27 +2,42 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLongestSubstring = function(s: string) {
+//   let count = 0
+//   let newS = s
+//   let tempStr = ""
+//   if(s === "") return 0
+//   if(s.length === 1) return 1
+//   while(newS.length > count) {
+//     for(let i = 0; i < newS.length; i++) {
+//       if(tempStr.indexOf(newS[i]) === -1) {
+//         tempStr += newS[i]        
+//         if(count < tempStr.length){
+//           count = tempStr.length
+//         }
+//       } else {
+//         newS = newS.substr(tempStr.indexOf(newS[i]) + 1)
+//         tempStr = ""
+//         break
+//       } 
+//     }
+//   }
+//   return count
+// };
+
 var lengthOfLongestSubstring = function(s: string) {
-  let count = 0
-  let newS = s
-  let tempStr = ""
-  if(s === "") return 0
-  if(s.length === 1) return 1
-  while(newS.length > count) {
-    for(let i = 0; i < newS.length; i++) {
-      if(tempStr.indexOf(newS[i]) === -1) {
-        tempStr += newS[i]        
-        if(count < tempStr.length){
-          count = tempStr.length
-        }
-      } else {
-        newS = newS.substr(tempStr.indexOf(newS[i]) + 1)
-        tempStr = ""
-        break
-      } 
+  let i = 0, j = 0, max = 0, map:{[key: string]: number} = {}
+  let n = s.length
+  while(i < n && j < n) {
+    if(map[s[j]] === undefined){
+      map[s[j]] = 1
+      j++
+      max = Math.max(max, j - i)
+    } else {
+      delete map[s[j]]
+      i++
     }
   }
-  return count
-};
-
-console.log(lengthOfLongestSubstring('ccc'))
+  return max
+}
+console.log(lengthOfLongestSubstring('pwwkew'))
